194cf099460c43fa57ab53d428f77b96a6ba7657
jaredl85 <jaredloveridge@gmail.com>
2021-09-02 17:51:18 -0700

    mongoose part 3

--- index.js
+++ index.js
@@ -2,25 +2,44 @@ const mongoose = require('mongoose');
 const Campsite = require('./models/campsite');
 
 const url = 'mongodb://localhost:27017/nucampsite';
+
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 connect.then(() => {
-    console.log("Connected correctly to the server");
+
+    console.log('Connected correctly to server');
 
     Campsite.create({
-        name: "React Lake Campground",
-        description: "test"
+        name: 'React Lake Campground',
+        description: 'test'
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
